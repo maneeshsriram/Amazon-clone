ee53e35dd49cd0f4270ce896f52a495eacd00368
Maneesh <maneeshsriram123@gmail.com>
2020-11-03 11:32:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,6 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJY5zGetCRBcbSnVa8K_bEsplk5lCF3e8",
-    authDomain: "amazn-clone-by-maneesh.firebaseapp.com",
-    databaseURL: "https://amazn-clone-by-maneesh.firebaseio.com",
-    projectId: "amazn-clone-by-maneesh",
-    storageBucket: "amazn-clone-by-maneesh.appspot.com",
-    messagingSenderId: "284497157529",
-    appId: "1:284497157529:web:4556c2cac31136036f4708",
-    measurementId: "G-4CJ3JY3G6W"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
